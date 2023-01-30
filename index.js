776f05555961a9f08952cd68da53f69684734d30
iSo3ndowed <100690895+iSo3ndowed@users.noreply.github.com>
2023-01-30 20:17:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const navMenu = document.querySelector('.hidden-menu')
+const menuBtn = document.getElementsByClassName('.fa-bars')
+const closeMenu = document.getElementsByClassName('.fa-xmark')
+
+
+
+function openMenu(){
+    navMenu.style.display = 'block'
+}
+function removeMenu(){
+    navMenu.style.display = 'none'
+}
